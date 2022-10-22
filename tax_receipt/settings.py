f7fde3a1d4e4b62e42fff0828a671ad010fcace0
art5551 <106573317+art5551@users.noreply.github.com>
2022-10-22 08:37:18 -0500

    Update settings.py

--- tax_receipt/settings.py
+++ tax_receipt/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pcvmdem__-ybk-6_&yg@vgl#w=l6z!t3%d(ei)+&0)idw$r76b'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
